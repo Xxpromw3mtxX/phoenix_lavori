c22dc71a32d56c862f3f6897f8a5a1f3cf86e6eb
Francesco Metta <fra.metta@hotmail.it>
2021-08-12 17:46:19 +0200

    new manifest file

---
+++ phoenix_lavori/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'lilfraae'
+description 'phoenix_lavori Modern and Responsive way to non-whitelisted jobs!'
+version '1.0.0'
+
+server_scripts {
+    'server/jobs/*.lua',
+    'server/sv_script.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/jobs/*.lua',
+    'client/cl_script.lua',
+    'config.lua'
+}
+
+shared_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua'
+}
+
+dependencies {
+	'es_extended'
+}
